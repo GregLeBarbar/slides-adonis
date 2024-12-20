63492cf06463b2ef0f1e9da1043173379abd2abb
Grégory Charmier <px86dym@etmlnet.local>
2024-12-20 09:26:27 +0100

    Updates

---
+++ assets/slidev/default-F5tJNPEJ.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-BWWh_rRC.js";import{_ as r}from"../index-BJvYw6UR.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
